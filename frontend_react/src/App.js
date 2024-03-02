395e8c6939fc3be3ba931acc4fc34a78fb7a30d2
Patryk Rogala <patryk@astarium.pl>
2024-03-02 13:19:55 +0100

    Add Navbar, Header, About, Work, Skills, Testimonial, and Footer components

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -1,8 +1,19 @@
 import React from 'react'
 
+import { About, Footer, Header, Skills, Testimonial, Work } from './container'
+import { Navbar } from './components'
+
 const App = () => {
   return (
-    <div>App</div>
+    <div className="app">
+      <Navbar />
+      <Header />
+      <About />
+      <Work />
+      <Skills />
+      <Testimonial />
+      <Footer />
+    </div>
   )
 }
 
