e9e3a2e8c5ca33ec469b3716a2392e9c2a570e6f
Patryk Rogala <rogala.patryk@pm.me>
2024-03-03 12:43:17 +0100

    Update Sanity client configuration

--- frontend_react/src/client.js
+++ frontend_react/src/client.js
@@ -1,12 +1,18 @@
 import sanityClient from '@sanity/client';
 import imageUrlBuilder from '@sanity/image-url';
 
+const { REACT_APP_SANITY_PROJECT_ID, REACT_APP_SANITY_TOKEN } = process.env;
+
+if (!REACT_APP_SANITY_PROJECT_ID || !REACT_APP_SANITY_TOKEN) {
+  throw new Error('Sanity project ID and token must be set in environment variables');
+}
+
 export const client = sanityClient({
-  projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
+  projectId: REACT_APP_SANITY_PROJECT_ID,
   dataset: 'production',
   apiVersion: '2022-02-01',
   useCdn: true,
-  token: process.env.REACT_APP_SANITY_TOKEN,
+  token: REACT_APP_SANITY_TOKEN,
 });
 
 const builder = imageUrlBuilder(client);
