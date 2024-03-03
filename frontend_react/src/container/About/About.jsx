b1800d3f3fc83154dc001a56276707356f6c5a99
Patryk Rogala <rogala.patryk@pm.me>
2024-03-03 14:46:56 +0100

    Remove unused import statement in About.jsx

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect} from 'react'
 import { motion } from 'framer-motion'
 
-import { images } from '../../constants'
 import './About.scss'
 import { urlFor, client } from '../../client'
 
